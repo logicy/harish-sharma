31a7e65564ce2efd54e3fbaaf2ade87189c9e7cd
sushiljainam <sushiljain1993@gmail.com>
2016-10-13 16:37:52 +0530

    theme updated

---
+++ wp-content/plugins/for_profilo.php
@@ -0,0 +1,45 @@
+<?php 
+/**
+ * @package post_types
+ * @project profilo
+ * @version 2.1
+ */
+/*
+Plugin Name: home para as Post
+Plugin URI: http://logicy.in/?ref=profilo&plugin=para
+Description: This plugin enables you to add para as Post.
+Author: logicY
+Author URI: http://logicy.in
+Version: 2.1
+Text Domain: para
+*/
+
+
+add_action( 'init', 'create_post_type_home_para' );
+function create_post_type_home_para() {
+  register_post_type( 'para',
+    array(
+      'labels'          => array(
+        'name'              => __( 'home para' ),
+        'singular_name'     => __( 'home para' ),
+        'add_new_item'      => __( 'Add home para' ),
+        'new_item'          => __( 'New home para' ),
+        'view_item'         => __( 'View home para' ),
+        'all_items'         => __( 'All home para' ),
+        'insert_into_item'  => __( 'Insert into home para' ),
+        'search_items'      => __( 'Search home para' ),
+        'not_found'         => __( 'No home para found' ),
+        'not_found_in_trash'=> __( 'No home para found in trash' )
+      ),
+      'menu_position'   => 5,
+      'menu_icon'       => 'dashicons-megaphone',//url or dashicon here for icon.png
+      'description'     => 'home para items for your website/products.',
+      'supports'        => array(//to add various supports: for featured-images=thumbnails
+          'title','editor','author'/*,'thumbnail'*/,'custom-fields'
+      ),
+      'taxonomies'      => array('category'),
+      'public'          => true,
+      'has_archive'     => true,
+    )
+  );
+}
